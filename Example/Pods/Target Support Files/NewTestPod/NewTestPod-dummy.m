0b1222af6df8cc205aeacc0f02543acfa3a170ea
ningmengjean <woshizhuxiaojin@sina.com>
2021-05-09 12:56:13 +0800

    Initial commit with cocoapod lib template

---
+++ Example/Pods/Target Support Files/NewTestPod/NewTestPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NewTestPod : NSObject
+@end
+@implementation PodsDummy_NewTestPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-NewTestPod_Example/Pods-NewTestPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NewTestPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NewTestPod_Example
+@end
